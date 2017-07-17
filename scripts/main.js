e0920a6b7f9548321ffd257e6961914ba975f441
Bradley Ryan <brad.conl3y@gmail.com>
2017-07-17 08:43:00 -0400

    completed blackjack

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,27 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
-
-
-  return;
+  cardValues = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K" : 10 }
+  totalValue = 0
+  for (var i = 0; i < hand.length; i++) {
+    if (hand[i] === "A" && totalValue <= 10) {
+      totalValue += 11
+    } else if (hand[i] === "A" && totalValue > 10) {
+      totalValue += 1
+    } else {
+      totalValue += cardValues[hand[i]]
+    }
+  }
+  if (totalValue > 21 && hand.includes("A")) {
+    totalValue -= 10
+  }
+  return totalValue
 }
 
 
